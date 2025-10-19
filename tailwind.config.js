43957cb9acd9e4f34a432addd8c14835059d14fa
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-10-19 21:47:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-// ---------- tailwind.config.js ----------
+/** @type {import('tailwindcss').Config} */
 export default {
-content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-theme: { extend: {} },
-plugins: [],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
